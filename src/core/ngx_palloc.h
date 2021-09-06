bb95659353d38a8be9abff8cbde9515547e421b8
ZJFans <1062004994@qq.com>
2021-09-06 05:22:50 -0700

    src/core/ngx_palloc.h

--- src/core/ngx_palloc.h
+++ src/core/ngx_palloc.h
@@ -31,37 +31,40 @@ typedef void (*ngx_pool_cleanup_pt)(void *data);
 
 typedef struct ngx_pool_cleanup_s  ngx_pool_cleanup_t;
 
-struct ngx_pool_cleanup_s {
-    ngx_pool_cleanup_pt   handler;
-    void                 *data;
-    ngx_pool_cleanup_t   *next;
+/* 自定义清理回调的数据结构   1. 每个work线程都会有一个“ 大 ”内存池  
+                            2.当有一个connection时，会在“大”的内存池中创建“ 中 ”内存池  
+   大 -  中  -  小           3.当这个连接有一个request时，会在“ 中 ”的内存池创建小的内存池 */
+                            
+    ngx_pool_cleanup_pt   handler;  /* 清理内存的回调函数 */
+    void                 *data;     /* 指向需要清理的数据地址 */
+    ngx_pool_cleanup_t   *next;     /* 指向下一个清理结构 */
 };
 
 
 typedef struct ngx_pool_large_s  ngx_pool_large_t;
 
 struct ngx_pool_large_s {
-    ngx_pool_large_t     *next;
-    void                 *alloc;
+    ngx_pool_large_t     *next;   /* 指向下一块存储大数据内存块的指针 */
+    void                 *alloc;  /* 指向本块内存的指针？？？？ */
 };
 
 
 typedef struct {
-    u_char               *last;
-    u_char               *end;
-    ngx_pool_t           *next;
-    ngx_uint_t            failed;
-} ngx_pool_data_t;
+    u_char               *last;    /* 当前内存池未使用内存初始地址 */
+    u_char               *end;     /* 内存池的结束地址 */
+    ngx_pool_t           *next;    /* 指向下一个内存池的指针 */
+    ngx_uint_t            failed;  /* 失败次数 ---- 分配内存时失败的次数 */
+}  ngx_pool_data_t;
 
 
 struct ngx_pool_s {
-    ngx_pool_data_t       d;
-    size_t                max;
-    ngx_pool_t           *current;
-    ngx_chain_t          *chain;
-    ngx_pool_large_t     *large;
-    ngx_pool_cleanup_t   *cleanup;
-    ngx_log_t            *log;
+    ngx_pool_data_t       d;        /* 内存池数据区域 */ 
+    size_t                max;      /* 最大每次可分配的内存值 */
+    ngx_pool_t           *current;  /* 指向当前内存池地址 */
+    ngx_chain_t          *chain;    /* 缓冲区链表 */
+    ngx_pool_large_t     *large;    /* 存储大数据的链表 */
+    ngx_pool_cleanup_t   *cleanup;  /* 清除内存块分配的内存 */
+    ngx_log_t            *log;      /* 日志 */
 };
 
 
